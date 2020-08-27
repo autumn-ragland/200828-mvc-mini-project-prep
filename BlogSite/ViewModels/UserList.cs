2805624762a40088fcc374f8c006ab8d00154feb
autumn-ragland <raglanda998@gmail.com>
2020-08-27 11:29:26 -0500

    user partial with styling displaying data

--- BlogSite/ViewModels/UserList.cs
+++ BlogSite/ViewModels/UserList.cs
@@ -9,7 +9,7 @@ namespace BlogSite.ViewModels
         {
             this.mockUsers.Add(new User("User", "One", "user1", "This is User One!", new List<Blog>(){new Blog("My First Blog Post", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")}, true));
 
-            this.mockUsers.Add(new User("User", "Two", "user2", "This is User Two!", new List<Blog>(){new Blog("Welcome to My Blog", " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")}, false));
+            this.mockUsers.Add(new User("User", "Two", "user2", "This is User Two!", new List<Blog>(){new Blog("Welcome to My Blog", " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."), new Blog("Wow My Second Blog Post", "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur")}, false));
         }
     }
 }
